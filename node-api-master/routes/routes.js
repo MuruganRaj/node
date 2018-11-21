1b5722f7acfa467b156779d0c996445c58fee609
Murugan <muruga4046@gmail.com>
2018-11-21 22:46:41 +0530

    group

--- node-api-master/routes/routes.js
+++ node-api-master/routes/routes.js
@@ -578,7 +578,7 @@ if(err){
  res.send(403);
 
 }else{
-			var query = pool.query("select * ,Date_add(created_date, INTERVAL 1 DAY)  as  expirydate ,timediff('24:00:00',time_format(timediff(current_timestamp(),created_date) ,'%H:%i:%s')) as timecount from Create_group cg join customers cs on (cg.customer_id=cs.CustomerID) where payment_status='N' and no_multy='0' order by group_id desc", function(err,rows){
+			var query = pool.query("select * ,Date_add(created_date, INTERVAL 1 DAY)  as  expirydate ,timediff('24:00:00',time_format(timediff(current_timestamp(),created_date) ,'%H:%i:%s')) as timecount from Create_group cg join customers cs on (cg.customer_id=cs.CustomerID) where payment_status='N' and no_multy='0' and group_status = 'Active'  order by group_id desc", function(err,rows){
 
 if(err){
         console.log("err"+err);
@@ -692,7 +692,7 @@ if(err){
  res.send(403);
 
 }else{
-        var query = pool.query("select count(group_id) as group_count from Create_group where payment_status ='N'",function(err,rows){
+        var query = pool.query("select count(group_id) as group_count from Create_group where payment_status ='N' and group_status ='Active' ",function(err,rows){
 
 if(err){
         console.log("err"+err);
