315da2f6b5a5f7b97e761cfcbbbbe83f3fd9e809
Murugan <muruga4046@gmail.com>
2018-12-02 12:51:51 +0530

    Update routes.js

--- node-api-master/routes/routes.js
+++ node-api-master/routes/routes.js
@@ -1100,7 +1100,7 @@ app.get('/api/v1/getJoinUserImages',ensureToken,function(req,res){
 
         }else{
 
-var query = pool.query('select c.ProfileImage from join_group j  join customers c on (j.customer_id=c.CustomerID) where group_id="'+group_id+'"',function(err,rows){
+var query = pool.query('select c.CustomerID, c.ProfileImage from join_group j  join customers c on (j.customer_id=c.CustomerID) where group_id="'+group_id+'"',function(err,rows){
 
 
    if(err){
