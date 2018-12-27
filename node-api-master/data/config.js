34733a13f53d9229fa1fc5b7530bd93c0d8d2556
Murugan <muruga4046@gmail.com>
2018-12-27 14:08:27 +0530

    Update config.js

--- node-api-master/data/config.js
+++ node-api-master/data/config.js
@@ -2,15 +2,14 @@ const mysql = require('mysql');
 //.us-east-2.rds.amazonaws.com
 // Set database connection credentials
 const config = {
-	host:'',
+	host:'molcsec.cdvkpp4t1tij.us-east-2.rds.amazonaws.com',
 user:'root',
 password:'admin123',
 port:3306,
-database:''
+database:'molc'
 
 	
 };
-
 
 // Create a MySQL pool
 const pool = mysql.createPool(config);
