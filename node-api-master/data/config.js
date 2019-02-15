b7a92badf5e5dc5bd67c1f264fcf99ef87d12c27
Murugan <muruga4046@gmail.com>
2019-02-15 20:13:11 +0530

    Update config.js

--- node-api-master/data/config.js
+++ node-api-master/data/config.js
@@ -3,11 +3,6 @@ const mysql = require('mysql');
 // Set database connection credentials
 const config = {
 	
-host:'molcdev.cdvkpp4t1tij.us-east-2.rds.amazonaws.com',
-user:'root',
-password:'admin123',
-port:3306,
-database:'molc_test_new'
 
 	
 };
