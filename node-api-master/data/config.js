bd608a1023e6b55c34f1833421b1cc125c7f77a1
Murugan <muruga4046@gmail.com>
2019-03-06 22:18:40 +0530

    Update config.js

--- node-api-master/data/config.js
+++ node-api-master/data/config.js
@@ -2,7 +2,7 @@ const mysql = require('mysql');
 //.us-east-2.rds.amazonaws.com
 // Set database connection credentials
 const config = {
-host:'139.59.57.126',
+host:'139.59.57',
 user:'root',
 password:'admin123',
 port:3306,
